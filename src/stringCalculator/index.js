ac72cf7c2449a588edd3e5c23c8f7f282722429e
sahaseb <sahasebook2@gmail.com>
2025-01-29 23:21:26 +0530

    task complition

--- src/stringCalculator/index.js
+++ src/stringCalculator/index.js
@@ -1,17 +1,14 @@
 function add(numbers) {
-	try {
-		const allNumbers = numbers.split(/,|;|\n/).map(Number)?.forEach(num =>{
-			console?.log(num,"dasasd")
-		});
-  
-  return allNumbers;
-		allNumber = numbers?.split("/,|\n/;").map(Number).forEach(num => num);
-		console?.log(allNumber,"allNumber");
-		// if(?.reduce((sum, num) => sum + Number(num), 0))
-		// return !numbers && 0 || numbers?.split(",")?.reduce((sum, num) => sum + Number(num), 0);
-	} catch (error) {
-		console?.log(error,"Failed during execution.")
-	}
+    if (!numbers) return 0;
+
+    let [delim, nums] = numbers.startsWith("//") ? [new RegExp(numbers[2]), numbers.split("\n")[1]] : [/[,\n]/, numbers];
+    let numArray = nums.split(delim).map(Number);
+    let negatives = numArray.filter(n => n < 0);
+    if (negatives.length) throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
+
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
+
+
 
 module.exports = add;
--- test/stringCalculatorTest/index.test.js
+++ test/stringCalculatorTest/index.test.js
@@ -3,3 +3,4 @@ const add = require("../../src/stringCalculator");
 test(" 1,5 string return 6" , () => { expect( add("1,5") ).toBe(6); });
 test(" 1\n2,3 string return 6" , () => { expect( add("1\n2,3") ).toBe(6); });
 test("//;\n1;2 should return 3", () => { expect( add("//;\n1;2") ).toBe(3); });
+test(" -1,-2,0,1,2 should return exception", () => { expect( add("-1,-2,0,1,2") ).toBe(3); });
